aa301f619ee11c3a22379d5c6fc0f79fcf4f628b
Sardar <sardarhomatv@gmail.com>
2024-09-23 10:11:54 +0300

    Loading added 2

--- src/components/LoadingCards.jsx
+++ src/components/LoadingCards.jsx
@@ -42,6 +42,46 @@ function LoadingCards() {
                     </div>
                 </div>
             </div>
+            <div class="Loading-cards">
+                <div class="container">
+                    <div class="loadin-card">
+                        <div class="skeleton-title"></div>
+                        <div class="skeleton-category"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="Loading-cards">
+                <div class="container">
+                    <div class="loadin-card">
+                        <div class="skeleton-title"></div>
+                        <div class="skeleton-category"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="Loading-cards">
+                <div class="container">
+                    <div class="loadin-card">
+                        <div class="skeleton-title"></div>
+                        <div class="skeleton-category"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="Loading-cards">
+                <div class="container">
+                    <div class="loadin-card">
+                        <div class="skeleton-title"></div>
+                        <div class="skeleton-category"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="Loading-cards">
+                <div class="container">
+                    <div class="loadin-card">
+                        <div class="skeleton-title"></div>
+                        <div class="skeleton-category"></div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
